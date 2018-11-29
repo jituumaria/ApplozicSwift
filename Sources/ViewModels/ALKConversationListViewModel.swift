7c537f04f2447c0c3a81543e406a7dcfd13ce298
shivampokhriyal <shivampokhriyal1997@gmail.com>
2018-11-29 19:48:43 +0530

    Separate tableview from ConversationListViewController

--- Sources/ViewModels/ALKConversationListViewModel.swift
+++ Sources/ViewModels/ALKConversationListViewModel.swift
@@ -16,8 +16,72 @@ protocol ALKConversationListViewModelDelegate: class {
     func rowUpdatedAt(position: Int)
 }
 
+/**
+ The `ConversationListViewModelProtocol` protocol defines the common interface though which an object provides message list information to an instance of `ConversationListTableViewController`.
+ 
+ A concrete class that conforms to this protocol is provided in the SDK. See `ALKConversationListViewModel`.
 
-final public class ALKConversationListViewModel: NSObject {
+ */
+public protocol ConversationListViewModelProtocol: class {
+    
+    /**
+     This method is used to determine the number of sections in the tableView.
+     - Returns: The number of sections in the tableView
+    */
+    func numberOfSection() -> Int
+    
+    /**
+     This method is used to determine the number of rows in a particular tableview section.
+     - Parameter section: Section of the tableView.
+     - Returns: The number of rows in `section`
+    */
+    func numberOfRowsInSection(section: Int) -> Int
+    
+    /**
+     This method is used to determine the message object at a particular indexPath of tableView.
+     - Parameter indexPath: IndexPath of the current tableView cell.
+     - Returns: An object that conforms to ALKChatViewModelProtocol for the required indexPath.
+     */
+    func chatForRow(indexPath: IndexPath) -> ALKChatViewModelProtocol?
+    
+    /**
+     This method is used to determine the complete message list.
+     - Returns: An array of all the messages in the list.
+    */
+    func getChatList() -> [Any]
+    
+    /**
+     This method is used to remove a message from the message list.
+     - Parameter message: The message object to be removed from the message list.
+    */
+    func remove(message: ALMessage)
+    
+    /**
+     This method is used to mute a particular conversation thread.
+     - Parameters:
+        - conversation: The message object whose conversation is to be muted.
+        - tillTime: NSNumber determining the amount of time conversation is to be muted.
+        - withCompletion: Escaping Closure when the mute request is complete.
+    */
+    func sendMuteRequestFor(conversation: ALMessage, tillTime: NSNumber, withCompletion: @escaping (Bool)->())
+    
+    /**
+     This method is used to unmute a particular conversation thread.
+     - Parameters:
+     - conversation: The message object whose conversation is to be unmuted.
+     - withCompletion: Escaping Closure when the unmute request is complete.
+     */
+    func sendUnmuteRequestFor(conversation: ALMessage, withCompletion: @escaping (Bool) -> ())
+    
+    /**
+     This method is used to fetch more messages from db if present.
+     - Parameter dbService: An object of `ALMessageDBService` to complete the fetching operation
+     */
+    func fetchMoreMessages(dbService: ALMessageDBService)
+}
+
+
+final public class ALKConversationListViewModel: NSObject, ConversationListViewModelProtocol {
 
     weak var delegate: ALKConversationListViewModelDelegate?
 
@@ -28,19 +92,19 @@ final public class ALKConversationListViewModel: NSObject {
         dbService.getMessages(nil)
     }
 
-    func getChatList() -> [Any] {
+    public func getChatList() -> [Any] {
         return allMessages
     }
 
-    func numberOfSection() -> Int {
+    public func numberOfSection() -> Int {
         return 1
     }
 
-    func numberOfRowsInSection(section: Int) -> Int {
+    public func numberOfRowsInSection(section: Int) -> Int {
         return allMessages.count
     }
 
-    func chatForRow(indexPath: IndexPath) -> ALKChatViewModelProtocol? {
+    public func chatForRow(indexPath: IndexPath) -> ALKChatViewModelProtocol? {
         guard indexPath.row < allMessages.count else {
             return nil
         }
@@ -51,7 +115,7 @@ final public class ALKConversationListViewModel: NSObject {
         return alMessage
     }
 
-    func remove(message: ALMessage) {
+    public func remove(message: ALMessage) {
         let messageToDelete = allMessages.filter { ($0 as? ALMessage) == message }
         guard let messageDel = messageToDelete.first as? ALMessage,
             let index = (allMessages as? [ALMessage])?.index(of: messageDel) else {
@@ -129,7 +193,7 @@ final public class ALKConversationListViewModel: NSObject {
         }
     }
 
-    func fetchMoreMessages(dbService: ALMessageDBService) {
+    public func fetchMoreMessages(dbService: ALMessageDBService) {
         guard !ALUserDefaultsHandler.getFlagForAllConversationFetched() else { return }
         delegate?.startedLoading()
         dbService.fetchConversationfromServer(completion: {
@@ -138,7 +202,7 @@ final public class ALKConversationListViewModel: NSObject {
         })
     }
     
-    func sendUnmuteRequestFor(conversation: ALMessage, withCompletion: @escaping (Bool) -> ()) {
+    public func sendUnmuteRequestFor(conversation: ALMessage, withCompletion: @escaping (Bool) -> ()) {
 
         let time = (Int(Date().timeIntervalSince1970) * 1000)
         sendMuteRequestFor(conversation: conversation, tillTime: time as NSNumber) { (success) in
@@ -146,7 +210,7 @@ final public class ALKConversationListViewModel: NSObject {
         }
     }
     
-    func sendMuteRequestFor(conversation: ALMessage, tillTime: NSNumber, withCompletion: @escaping (Bool)->()) {
+    public func sendMuteRequestFor(conversation: ALMessage, tillTime: NSNumber, withCompletion: @escaping (Bool)->()) {
         if conversation.isGroupChat, let channel = ALChannelService().getChannelByKey(conversation.groupId) {
             // Unmute channel
             let muteRequest = ALMuteRequest()
